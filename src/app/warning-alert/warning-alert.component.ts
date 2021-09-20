7d8ca3da0a96c6ac5cbd22092873fe410a340c03
mclark5215 <90718213+mclark5215@users.noreply.github.com>
2021-09-20 10:46:36 -0500

    ..

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+ selector: 'app-warning-alert',
+ template: '<p>Stranger Danger!</p>',
+//styles: [
+ //  0 '
+ //     p { 
+ //        padding: 20px; 
+ //        background-color: mistyrose; 
+ //        border: 1px solid red;
+ //     } 
+ //   ' 
+ // ] 
+})
+export class WarningAlertComponent {
+
+}
--- src/basics-components/src/app/app.component.ts
+++ src/basics-components/src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
- 
   templateUrl: './app.component.html',
-// styleUrls: ['./app.component.css']
- styles: [' h3 { color: dodgerblue; } ']
-
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  `]
 })
-
 export class AppComponent {
-  title = 'my-first-app';
 }
